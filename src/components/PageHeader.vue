0457262dc0897b57803f47480b3fd6aa45d1672f
MattiaAngelini <mattiaangelini1993@gmail.com>
2024-04-22 14:50:06 +0200

    Add: layout header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,16 +1,83 @@
 <script>
 
 export default {
-    name:'PageHeader'
+    name:'PageHeader',
+    
+ data() {
+
+    return {
+    // 
+    };
+    },
+    methods: {
+    //
+    },
+    mounted() {
+    //
+
+}
+
 }
 
 </script>
 
 <template>
-    HEADER
+
+    <header>
+
+        <section class="d-flex justify-content-center gap-3">
+
+            <!--LOGO-->
+            <div>
+                <img src="../assets/img/loader-autocar.png" alt="">
+                <span>Autocar</span>
+            </div>
+            
+            <!--NAV-->
+            <ul class="d-flex align-items-center gap-3">
+                <li>PROVE</li>
+                <li>PROVE</li>
+                <li>PROVE</li>
+                <li>PROVE</li>
+                <li>PROVE</li>
+                <li>PROVE</li>
+                <li>PROVE</li>
+            </ul>
+            
+            <!--ACCOUNT-->
+            <div>
+                <img src="../assets/img/loader-autocar.png" alt="">
+            </div>
+            
+            <!--ADD CAR-->
+            <div>
+                <img src="../assets/img/loader-autocar.png" alt="">
+            </div>
+            
+            <!--HAMBURGER-->
+            <div>
+                <img src="../assets/img/loader-autocar.png" alt="">
+            </div>
+            
+            <!--CALL CENTER NUMBER-->
+            <div>
+                <img src="../assets/img/loader-autocar.png" alt="">
+            </div>
+            
+        </section>
+
+
+    </header>
+
+
+
+
+
 </template>
 
 <style scoped lang="scss">
 @use '../src/assets/style/generic' as *;
+
+
    
 </style>
